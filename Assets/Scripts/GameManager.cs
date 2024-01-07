357b152cb4a4bd8d78f678b5cbfb553df59daf73
Doroddi <ibus7180@konkuk.ac.kr>
2024-01-07 11:45:58 +0900

    화면 전환 모듈 생성
    화면 전환 모듈 생성
    default : FadeO/I

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,31 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Net;
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance;
+    [SerializeField] SceneTransitionManager sceneTransitionManager;
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            sceneTransitionManager.NextLevel();
+        }
+    }
 }
--- Assets/Scripts/SceneTransitionManager.cs
+++ Assets/Scripts/SceneTransitionManager.cs
@@ -1,32 +1,23 @@
 using UnityEngine;
 using UnityEngine.UI;
-using System.Threading;
- 
+using System.Collections;
+using UnityEngine.SceneManagement;
+using static UnityEditor.Experimental.GraphView.Port;
+
 public class SceneTransitionManager : MonoBehaviour
 {
- 
-	public Image fadeImage;
-    public float fadeSpeed = 0.5f;
-    private bool fadeDir = true;
-    private void Awake(){
-        fadeImage = GetComponent<Image>();
-        Thread.Sleep(2000);
+    [SerializeField] Animator trasitionAnim;
+
+    public void NextLevel()
+    {
+        StartCoroutine(LoadLevel());
     }
 
-    private void Update()
+    IEnumerator LoadLevel()
     {
-        Color color = fadeImage.color;
-        if(fadeDir){
-            color.a += Time.deltaTime*fadeSpeed;
-            if(color.a>1) fadeDir =false;
-        }
-        else {
-            color.a -= Time.deltaTime*fadeSpeed;
-            if(color.a<0) fadeDir=true;
-        }
-
-        fadeImage.color = color;
-
-	}
- 
+        trasitionAnim.SetTrigger("out");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex + 1);
+        trasitionAnim.SetTrigger("in");
+    }
 }
