6f3527cffe78614f4b96782246aad922433cccef
ekgns33 <76658405+ekgns33@users.noreply.github.com>
2024-02-19 17:05:06 +0900

    feat : modify map design, add dialog to switch game

--- Assets/Scripts/Entity/GameLink.cs
+++ Assets/Scripts/Entity/GameLink.cs
@@ -24,12 +24,20 @@ public class GameLink : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(!isGameStart) {  
+        /**if(!isGameStart) {  
             RaycastHit2D hit = Physics2D.BoxCast(transform.position, interactableRange , 0, Vector2.up, 0f,LayerMask.GetMask("Player"));
             if(hit.collider != null && Input.GetKeyDown(KeyCode.E)) {
                 isGameStart = true;
                 WaterTankSceneManager.instance.SetupMiniGame(gameOrder);
             }   
+        }**/
+    }
+
+   public void StartGame() {
+        if(!isGameStart) {  
+            isGameStart = true;
+            WaterTankSceneManager.instance.SetupMiniGame(gameOrder);
+            
         }
     }
 
